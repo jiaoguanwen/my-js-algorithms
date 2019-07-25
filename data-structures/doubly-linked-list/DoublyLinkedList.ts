e742fbb926c5fe4b81cd209bf7dddf36af936241
jiaoguanwen <jiaoguanwen@163.com>
2019-07-26 01:00:27 +0800

    add some methods

--- data-structures/doubly-linked-list/DoublyLinkedList.ts
+++ data-structures/doubly-linked-list/DoublyLinkedList.ts
@@ -11,4 +11,127 @@ export default class DoublyLinkedList {
     this.tail = null;
     this.compare = new Comparator(comparatorFunction);
   }
+
+  public prepend(value: any): DoublyLinkedList {
+    const newNode = new DoublyLinkedListNode(value, this.head, null);
+    if (this.head) {
+      this.head.previous = newNode;
+    }
+    this.head = newNode;
+    if (!this.tail) {
+      // DO NOT POINT tail TO head, IT'S NOT OK
+      this.tail = newNode;
+    }
+    return this;
+  }
+
+  public append(value: any): DoublyLinkedList {
+    /* const newNode = new DoublyLinkedListNode(value, null, this.tail);
+    if (this.tail) {
+      this.tail.next = newNode;
+    }
+    this.tail = newNode;
+    if (!this.head) {
+      this.head = newNode;
+    }
+    return this; */
+    const newNode = new DoublyLinkedListNode(value, null, null);
+    if (!this.head) {
+      this.head = newNode;
+      this.tail = newNode;
+      return this;
+    }
+    this.tail.next = newNode;
+    newNode.previous = this.tail;
+    this.tail = newNode;
+    return this;
+  }
+
+  public delete(value: any): DoublyLinkedListNode | null {
+    /* if (!this.head) {
+      return null;
+    }
+    let currentNode = this.head;
+    while (currentNode) {
+      if (currentNode.value === value) {
+        if (!currentNode.next && !currentNode.previous) {
+          this.head = null;
+          this.tail = null;
+        } else if (!currentNode.previous) {
+          currentNode.next.previous = null;
+          this.head = currentNode.next;
+        } else if (!currentNode.next) {
+          currentNode.previous.next = null;
+          this.tail = currentNode.previous;
+        } else {
+          currentNode.previous.next = currentNode.next;
+          currentNode.next.previous = currentNode.previous;
+        }
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    } */
+    if (!this.head) {
+      return null;
+    }
+    let deletedNode = null;
+    let currentNode = this.head;
+    while (currentNode) {
+      if (this.compare.equal(currentNode.value, value)) {
+        deletedNode = currentNode;
+        if (deletedNode === this.head) {
+          this.head = currentNode.next;
+          if (this.head) {
+            this.head.previous = null;
+          }
+          if (deletedNode === this.tail) {
+            this.tail = null;
+          }
+        } else if (deletedNode === this.tail) {
+          this.tail = currentNode.previous;
+          this.tail.next = null;
+        } else {
+          const previousNode = currentNode.previous;
+          const nextNode = currentNode.next;
+          previousNode.next = nextNode;
+          nextNode.previous = previousNode;
+        }
+      }
+      currentNode = currentNode.next;
+    }
+    return deletedNode;
+  }
+
+  public find(value: any, callback?: Function): DoublyLinkedListNode | null {
+    /* if (!this.head) {
+      return null;
+    }
+    let targetNode = null;
+    let currentNode = this.head;
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        targetNode = currentNode;
+      }
+      if (this.compare.equal(currentNode.value, value)) {
+        targetNode = currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return targetNode; */
+    if (!this.head) {
+      return null;
+    }
+    let currentNode = this.head;
+    while (currentNode) {
+      if (callback && callback(currentNode.value)) {
+        return currentNode;
+      }
+      // when we use typescript, there will be a error when value is undefined
+      if (value !== undefined && this.compare.equal(currentNode.value, value)) {
+        return currentNode;
+      }
+      currentNode = currentNode.next;
+    }
+    return null;
+  }
 }
--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import LinkedList from './data-structures/linked-list/LinkedList';
+/* import LinkedList from './data-structures/linked-list/LinkedList';
 import LinkedListNode from './data-structures/linked-list/LinkedListNode';
 
 const ll = new LinkedList();
@@ -14,4 +14,19 @@ console.log(ll.delete(18));
 
 console.log(ll);
 console.log(ll.deleteTail());
+console.log(ll);
+ */
+
+import DoublyLinkedList from "./data-structures/doubly-linked-list/DoublyLinkedList";
+import DoublyLinkedListNode from "./data-structures/doubly-linked-list/DoublyLinkedListNode";
+
+const ll = new DoublyLinkedList();
+
+ll.prepend(1);
+ll.prepend(2);
+ll.append(3);
+ll.append(4);
+ll.append(5);
+ll.delete(5);
+
 console.log(ll);
