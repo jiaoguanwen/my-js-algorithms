69f90c2b8e712dcc4e44643765f222ffba269ddd
jiaoguanwen <jiaoguanwen@163.com>
2019-08-12 20:29:05 +0800

    add priority queue methods

--- data-structures/priority-queue/PriorityQueue.ts
+++ data-structures/priority-queue/PriorityQueue.ts
@@ -10,11 +10,43 @@ export default class PriorityQueue extends MinHeap {
     this.compare = new Comparator(this.comparePriority.bind(this));
   }
 
-  public add(item: any, priority = 0): PriorityQueue {
+  public add(item: any, priority: number = 0): PriorityQueue {
+    this.priorities.set(item, priority);
+    super.add(item);
     return this;
   }
 
+  public remove(item: any, customFindingComparator: Comparator): PriorityQueue {
+    super.remove(item, customFindingComparator);
+    this.priorities.delete(item);
+    return this;
+  }
+
+  public changePriority(item: any, priority: number): PriorityQueue {
+    this.remove(item, new Comparator(this.compareValue));
+    this.add(item, priority);
+    return this;
+  }
+
+  private findByValue(item: any): Array<number> {
+    return this.find(item, new Comparator(this.compareValue));
+  }
+
+  public hasValue(item: any): boolean {
+    return this.findByValue(item).length > 0;
+  }
+
   public comparePriority(a: any, b: any): number {
-    return 0;
+    if (this.priorities.get(a) === this.priorities.get(b)) {
+      return 0;
+    }
+    return this.priorities.get(a) < this.priorities.get(b) ? -1 : 1;
+  }
+
+  private compareValue(a: any, b: any): number {
+    if (a === b) {
+      return 0;
+    }
+    return a < b ? -1 : 1;
   }
 }
