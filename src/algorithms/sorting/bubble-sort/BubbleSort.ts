c406a835e51156c1471a29671351945c4c401a9e
jiaoguanwen <jiaoguanwen@163.com>
2019-08-19 10:58:07 +0800

    Create BubbleSort.ts

---
+++ src/algorithms/sorting/bubble-sort/BubbleSort.ts
@@ -0,0 +1,23 @@
+import Sort from '../Sort';
+
+export default class BubbleSort extends Sort {
+  public sort(originalArray: any): Array<any> {
+    let swapped = false;
+    const array = [...originalArray];
+    for (let i = 1; i < array.length; i += 1) {
+      swapped = false;
+      this.callbacks.visitingCallback(array[i]);
+      for (let j = 0; j < array.length - i; j += 1) {
+        this.callbacks.visitingCallback(array[j]);
+        if (this.comparator.lessThan(array[j + 1], array[j])) {
+          [array[j], array[j + 1]] = [array[j + 1], array[j]];
+          swapped = true;
+        }
+      }
+      if (!swapped) {
+        return array;
+      }
+    }
+    return array;
+  }
+}
